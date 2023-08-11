a543e91834089fe59041d8aed96a8e047d79dec2
prwt <perawit37@gmail.com>
2023-08-11 15:21:49 +0700

    Create class-compenents.js

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header, Content, Footer } from "./func-components";
+import Calendar from './class-components'
 
 function App() {
   return (
     <>
       <Header /> 
+      <p><center><Calendar/></center></p>
       <Content /> 
       <Footer />
     </>
