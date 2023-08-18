2f0f92fdb9a07c479a9c05ee1f49c7f21a657f3e
prwt <perawit37@gmail.com>
2023-08-18 15:24:15 +0700

    Create banner.js calculator.js event-data.js

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,16 +1,6 @@
 import React from "react";
-import { Header, Content, Footer } from "./func-components";
-import Calendar from './class-components'
+import { EventData2 } from "./event-data";
 
-function App() {
-  return (
-    <>
-      <Header /> 
-      <p><center><Calendar/></center></p>
-      <Content /> 
-      <Footer />
-    </>
-  );
+export default function App() {
+  return <EventData2 />;
 }
-
-export default App;
