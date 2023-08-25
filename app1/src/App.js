09679329b23e3c143e31ea8e737a02a8b710e449
prwt <perawit37@gmail.com>
2023-08-25 15:41:14 +0700

    Create state-func.js

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,6 +1,32 @@
-import React from "react";
-import Cart from "./state-class";
+import React from 'react';
+/*import { Header, Content, Footer } from './func-components';
+import {Calendar} from './class-components';
+import './App.css';
+import logo from './logo.svg';
+import './style.css';
+import Banner from './banner'
+import {Button} from './class-components';
+import {Calculator} from './calculator';
+import {Calculator2} from './calculator';
+import {EventData2} from './event-data';
+import {Table} from './event-data';*/
+import RefsFunc from './refs-func';
+import RefsArray from './refs-array';
+import MessageBox from './state-func';
 
-export default function App() {
-  return <Cart />;
+function App() {
+  return(
+        <>
+        <center>
+          <RefsFunc/>
+          <br/>
+          <RefsArray/>
+          <br/>
+          <MessageBox/>
+          <br/>
+        </center>
+        </>
+  ) 
 }
+
+export default App;
