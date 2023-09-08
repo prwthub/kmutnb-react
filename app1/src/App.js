34e325e8c42cc261753d162a529cf37c1032840f
perawit anansukatham <perawit37@gmail.com>
2023-09-08 14:22:17 +0700

    Create context.js context-header.js context-content.js

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 /*import { Header, Content, Footer } from './func-components';
 import {Calendar} from './class-components';
 import './App.css';
@@ -9,24 +9,19 @@ import {Button} from './class-components';
 import {Calculator} from './calculator';
 import {Calculator2} from './calculator';
 import {EventData2} from './event-data';
-import {Table} from './event-data';*/
+import {Table} from './event-data';
 import RefsFunc from './refs-func';
 import RefsArray from './refs-array';
-import MessageBox from './state-func';
-
-function App() {
-  return(
-        <>
-        <center>
-          <RefsFunc/>
-          <br/>
-          <RefsArray/>
-          <br/>
-          <MessageBox/>
-          <br/>
-        </center>
-        </>
-  ) 
+import MessageBox from './state-func';*/
+import { userContext } from "./context";
+import Header from "./context-header";
+import Content from "./context-content";
+export default function App() {
+  return (
+    <userContext.Provider value={"Tom Jerry"}>
+      {" "}
+      <Header />
+      <Content />
+    </userContext.Provider>
+  );
 }
-
-export default App;
