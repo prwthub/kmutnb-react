40636b66d8a7e4644c14d7d1d3d5b1b7c79e12f0
perawit anansukatham <perawit37@gmail.com>
2023-09-08 15:44:55 +0700

    class homework context2

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 /*import { Header, Content, Footer } from './func-components';
 import {Calendar} from './class-components';
 import './App.css';
@@ -14,14 +14,15 @@ import RefsFunc from './refs-func';
 import RefsArray from './refs-array';
 import MessageBox from './state-func';*/
 import { userContext } from "./context";
-import Header from "./context-header";
-import Content from "./context-content";
+import Header2 from './context-header2'
+import Content2 from './context-content2'
+
 export default function App() {
-  return (
-    <userContext.Provider value={"Tom Jerry"}>
-      {" "}
-      <Header />
-      <Content />
-    </userContext.Provider>
-  );
+    let[user,setUser]= React.useState('');
+    return (
+        <userContext.Provider value={[user,setUser]}>
+            <Header2/>
+            <Content2/>
+        </userContext.Provider>
+    ) 
 }
