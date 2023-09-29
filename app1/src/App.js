c097431b75ea566cbdb0c5cf4c7b76b4ce736a55
perawit anansukatham <perawit37@gmail.com>
2023-09-29 14:29:04 +0700

    Create form-search.js form-bootstarp.js form-text.js
    install bootstap
    npm install bootstarp

--- app1/src/App.js
+++ app1/src/App.js
@@ -15,14 +15,13 @@ import RefsArray from './refs-array';
 import MessageBox from './state-func';
 import { userContext } from "./context";
 import Header2 from './context-header2'
-import Content2 from './context-content2'*/
-import Router2 from "./router2"
-;
+import Content2 from './context-content2'
+import Router2 from "./router2";
 import Router3 from "./router3";
+import FormSearch from './form-search';
+import FormBootstrap from './form-bootstrap'*/
+import FormText from './form-text'
 
 export default function App() {
-    let[user,setUser]= React.useState('');
-    return (
-        <Router3/>
-    ) 
+    return <FormText/>
 }
