9f8eebbe76faf9550c5b1213ca63dcab66fc92c4
perawit anansukatham <perawit37@gmail.com>
2023-09-29 15:42:55 +0700

    Create form-check.js

--- app1/src/App.js
+++ app1/src/App.js
@@ -19,9 +19,10 @@ import Content2 from './context-content2'
 import Router2 from "./router2";
 import Router3 from "./router3";
 import FormSearch from './form-search';
-import FormBootstrap from './form-bootstrap'*/
-import FormText from './form-text'
+import FormBootstrap from './form-bootstrap'
+import FormText from './form-text'*/
+import FormCheck from "./form-check";
 
 export default function App() {
-    return <FormText/>
+    return <FormCheck/>
 }
