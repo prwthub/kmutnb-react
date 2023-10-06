8ae6535ead99fa138dff6d3a48aa4bf510d0b7c6
perawit anansukatham <perawit37@gmail.com>
2023-10-06 15:01:38 +0700

    Create form-validation.js

--- app1/src/App.js
+++ app1/src/App.js
@@ -20,9 +20,10 @@ import Router2 from "./router2";
 import Router3 from "./router3";
 import FormSearch from './form-search';
 import FormBootstrap from './form-bootstrap'
-import FormText from './form-text'*/
-import FormCheck from "./form-check";
+import FormText from './form-text'
+import FormCheck from "./form-check";*/
+import FormValidation from "./form-validation";
 
 export default function App() {
-    return <FormCheck/>
+    return <FormValidation/>
 }
