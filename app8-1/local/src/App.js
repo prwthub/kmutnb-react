b4039cfc557e023894d65d77f609153fd0bf1367
prwt <perawit37@gmail.com>
2023-10-16 16:17:00 +0700

    Crate local and server

--- app8-1/local/src/App.js
+++ app8-1/local/src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+//import RestBasic from './rest-basic'
+//import FormGet from './form-get'
+import FormPost from './form-post'
+export default function App() {
+  return <FormPost/>
 }
-
-export default App;
--- app8-1/server/index.js
+++ app8-1/server/index.js
@@ -1,16 +1,25 @@
-const http = require('http') 
+const express = require('express')
+const app = express()
 const port = 8000
-http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' }) 
-    response.write(`
-    <!doctype html> <html>
-    <head>
-        <title>Node.js</title> </head>
-    <body>
-        <h3>Welcome to Node.js</h3>
-        <b>Node.js runs JavaScript on Server-Side</b>
-    </body>
-    </html> `)
-    response.end()
-}).listen(port)
-console.log('Server listening on port ' + port)
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static('public'))
+
+app.post('/api/form-post', (request, response) => {
+    let name = request.body.name || ''
+    let email = request.body.email || ''
+    let header = request.body.header || ''
+    let msg = request.body.message || ''
+    let text = `
+        <table border="1">
+            <caption>ข้อมูลที่ส่งขึ้นไป</caption>
+            <tr><td>ชื่อ:</td><td>${name}</td></tr>
+            <tr><td>อีเมล:</td><td>${email}</td></tr>
+            <tr><td>หัวข้อ:</td><td>${header}</td></tr>
+            <tr><td>ข้อความ:</td><td>${msg}</td></tr>
+        </table>
+    `
+    response.send(text)
+})    
+
+app.listen(port, () => console.log('Server listening on port '+port))
